64e9d9f5af009df3de8a4dc284b7d3d11043d4ec
Egor-Matveev <egormsm@gmail.com>
2022-11-01 20:38:27 +0300

    add storybook tests, update screenshots

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,19 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
   const { t } = useTranslation();
-  const [value, setValue] = useState('');
-
-  const handleChange = (value: string) => {
-    setValue(value);
-  };
 
   return (
     <div>
       {t('Главная страница')}
-      <Input value={value} onChange={handleChange} placeholder="Введите текст" />
     </div>
   );
 };
