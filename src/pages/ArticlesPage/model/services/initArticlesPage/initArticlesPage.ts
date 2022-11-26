ec0e3e12fd4117e5231ed338b8d233bd2ac1b41c
Egor-Matveev <egormsm@gmail.com>
2022-11-26 18:12:53 +0300

    add init type from url

--- src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
+++ src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
 import { SortOrder } from 'shared/types';
-import { ArticleSortField } from 'entities/Article';
+import { ArticleSortField, ArticleType } from 'entities/Article';
 import { articlesPageActions } from '../../slice/artcliesPageSlice';
 import { fetchArticlesList } from '../fetchArticlesList/fetchArticlesList';
 import { getArticlesPageInited } from '../../selectors/articlePageSelectors';
@@ -18,6 +18,7 @@ export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkCon
     const orderFromUrl = searchParams.get('order') as SortOrder;
     const sortFromUrl = searchParams.get('sort') as ArticleSortField;
     const searchFromUrl = searchParams.get('search');
+    const typeFromUrl = searchParams.get('type') as ArticleType;
 
     if (orderFromUrl) {
       dispatch(articlesPageActions.setOrder(orderFromUrl));
@@ -29,6 +30,10 @@ export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkCon
 
     if (searchFromUrl) {
       dispatch(articlesPageActions.setSearch(searchFromUrl));
+    }
+
+    if (typeFromUrl) {
+      dispatch(articlesPageActions.setType(typeFromUrl));
     }
 
     dispatch(articlesPageActions.initState());
